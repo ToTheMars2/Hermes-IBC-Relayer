c09feba9fd34821785bb6b9040bf779c1a50a359
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-10-28 10:37:52 +0300

    Update create_config.sh

--- create_config.sh
+++ create_config.sh
@@ -1,3 +1,20 @@
+echo "
+export Chain_id_one=${Chain_id_one}
+export IP_server_one=${IP_server_one}
+export RPC_port_one=${RPC_port_one}
+export GRPC_port_one=${GRPC_port_one}
+export Prefix_one=${Prefix_one}
+export Denom_one=${Denom_one}
+
+export Chain_id_two=${Chain_id_two}
+export IP_server_two=${IP_server_two}
+export RPC_port_two=${RPC_port_two}
+export GRPC_port_two=${GRPC_port_two}
+export Prefix_two=${Prefix_two}
+export Denom_two=${Denom_two}
+" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
 echo "[global]
 log_level = 'info'
 [mode]
@@ -65,3 +82,5 @@ trusting_period = '14days'
 trust_threshold = { numerator = '1', denominator = '3' }
 address_type = { derivation = 'cosmos' }
 " > $HOME/.hermes/config.toml
+
+sed -i '/Chain_id\|IP_server\|RPC_port\|GRPC_port\|Prefix\|Denom/d' $HOME/.bash_profile
