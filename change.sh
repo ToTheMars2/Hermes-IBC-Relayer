776e513c7292065d90e749b24b575d2e20c1176c
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-10-30 17:29:08 +0200

    Create change.sh

---
+++ change.sh
@@ -0,0 +1,15 @@
+echo "
+export Chain_id_one=''
+export IP_server_one=''
+export RPC_port_one=''
+export GRPC_port_one=''
+export Prefix_one=''
+export Denom_one=''
+
+export Chain_id_two=''
+export IP_server_two=''
+export RPC_port_two=''
+export GRPC_port_two=''
+export Prefix_two=''
+export Denom_two=''" >> $HOME/.bash_profile
+source $HOME/.bash_profile
