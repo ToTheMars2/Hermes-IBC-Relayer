55e5326b2271f43520f8938a33e6338c5450046d
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-10-30 16:10:23 +0200

    Update create_config.sh

--- create_config.sh
+++ create_config.sh
@@ -1,18 +1,3 @@
-echo "
-export Chain_id_one=${Chain_id_one}
-export IP_server_one=${IP_server_one}
-export RPC_port_one=${RPC_port_one}
-export GRPC_port_one=${GRPC_port_one}
-export Prefix_one=${Prefix_one}
-export Denom_one=${Denom_one}
-
-export Chain_id_two=${Chain_id_two}
-export IP_server_two=${IP_server_two}
-export RPC_port_two=${RPC_port_two}
-export GRPC_port_two=${GRPC_port_two}
-export Prefix_two=${Prefix_two}
-export Denom_two=${Denom_two}" >> $HOME/.bash_profile
-source $HOME/.bash_profile
 
 echo "[global]
 log_level = 'info'
