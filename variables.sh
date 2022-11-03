39d959a6167e938da8ff9a656be7f0f2844c3c88
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-11-03 09:00:23 +0200

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,13 @@
+Bin="" # hid-noded 
+Cosmos_addr="" # hid1..... 
+Name_wallet="" # tothemars 
+Chain_id=""  # jagrat 
+Node=""  # http://localhost:26657
+
+echo "export Bin="" # hid-noded
+export Cosmos_addr="" # hid1.....
+export Name_wallet="" # tothemars
+export Chain_id_one="" # jagrat
+export Chain_id_two="" # theta-testnet-001
+export Node=""  # http://localhost:26657" >> $HOME/.bash_profile
+echo "Зміні створені"
